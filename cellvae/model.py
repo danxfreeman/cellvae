835f5301530a08cf8ad40061e179380e2b714053
Freeman <daf179@login04.o2.rc.hms.harvard.edu>
2024-12-16 10:29:45 -0500

    Use dummy variable to evaluate linear input size

--- cellvae/model.py
+++ cellvae/model.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 
 class CellCNN(nn.Module):
@@ -5,20 +6,26 @@ class CellCNN(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.config = config
-        conv_output_size = 6272 # TODO: calculate dynamically
-        self.layers = nn.Sequential(
+        self.conv_layers = nn.Sequential(
             nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=0),
             nn.BatchNorm2d(16),
             nn.ReLU(),
             nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=0),
             nn.BatchNorm2d(32),
             nn.ReLU(),
-            nn.Flatten(),
-            nn.Linear(conv_output_size, 128),
+            nn.Flatten()
+        )
+        with torch.no_grad():
+            dummy = torch.zeros(1, 3, self.config.preprocess.crop_size, self.config.preprocess.crop_size)
+            fc_input = self.conv_layers(dummy).numel()
+        self.fc_layers = nn.Sequential(
+            nn.Linear(fc_input, 128),
             nn.ReLU(),
             nn.Linear(128, 1),
             nn.Sigmoid()
         )
     
     def forward(self, x):
-        return self.layers(x)
+        x = self.conv_layers(x)
+        x = self.fc_layers(x)
+        return x
