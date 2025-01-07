0594b4ed434e68fb024dba8da4884f7be6d7818f
Dan Freeman1 <dan@terraflow.app>
2025-01-07 10:30:35 -0500

    Order private methods after public methods

--- cellvae/plot.py
+++ cellvae/plot.py
@@ -9,6 +9,24 @@ class Plot:
     def __init__(self, config, dataset=None):
         self.config = config
         self.dataset = dataset
+    def cell(self, id_, **kwargs):
+        _, ax = plt.subplots()
+        self._plot_cell(ax, id_=id_, **kwargs)
+        plt.show()
+
+    def gallery(self, ids, cols=None, rows=None, **kwargs):
+        N = len(ids)
+        cols = cols or int(np.ceil(np.sqrt(N)))
+        rows = rows or int(np.ceil(N / cols))
+        _, axs = plt.subplots(rows, cols, figsize=(cols * 2, rows * 2))
+        axs = np.array(axs).flatten()
+        for i, ax in enumerate(axs):
+            if i < N:
+                self._plot_cell(ax=ax, id_=ids[i], **kwargs)
+            else:
+                ax.set_visible(False)
+        plt.tight_layout()
+        plt.show()
     
     def _plot_cell(self, ax, id_, window=None, outline=False):
         window = window or self.config.preprocess.crop_size
@@ -28,24 +46,7 @@ class Plot:
                 alpha=0.5
             )
             ax.add_patch(circle)
-    
-    def cell(self, id_, **kwargs):
-        _, ax = plt.subplots()
-        self._plot_cell(ax, id_=id_, **kwargs)
-        plt.show()
 
-    def grid(self, ids, cols=None, rows=None, **kwargs):
-        N = len(ids)
-        cols = cols or int(np.ceil(np.sqrt(N)))
-        rows = rows or int(np.ceil(N / cols))
-        _, axs = plt.subplots(rows, cols, figsize=(cols * 2, rows * 2))
-        axs = np.array(axs).flatten()
-        for i, ax in enumerate(axs):
-            if i < N:
-                self._plot_cell(ax=ax, id_=ids[i], **kwargs)
-            else:
-                ax.set_visible(False)
-        plt.tight_layout()
         plt.show()
     
     def loss(self):
