0eb434914b6e591295bf1ee3e900aa0f85a6e6cf
Dan Freeman <danxfreeman@gmail.com>
2025-02-26 09:03:01 -0500

    Embed custom loader

--- cellvae/agent.py
+++ cellvae/agent.py
@@ -107,12 +107,13 @@ class CellAgent:
         sum_loss = mse_loss + (kld_loss * self.config.model.beta)
         return mse_loss, kld_loss, sum_loss
 
-    def embed(self):
+    def embed(self, loader):
         """Embed validation set."""
         self.model.eval()
         with torch.no_grad():
-            for i, x in enumerate(self.loader.valid_loader):
-                logging.info(f'Embedding batch {i} of {len(self.loader.valid_loader)}.')    
+            for i, x in enumerate(loader):
+                logging.info(f'Embedding batch {i} of {len(loader)}.')
+                x = x.to(self.device)
                 x_hat_batch, z_batch, _ = self.model(x)
                 for x_hat, z in zip(x_hat_batch, z_batch):
                     yield x_hat, z
