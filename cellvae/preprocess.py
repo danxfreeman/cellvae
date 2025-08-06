7c793e041d6506b97870e534f554d1d01f298fff
Dan Freeman <danxfreeman@gmail.com>
2025-08-06 09:44:53 -0400

    Transform thumbnails during preprocessing

--- cellvae/preprocess.py
+++ cellvae/preprocess.py
@@ -5,19 +5,23 @@ import numpy as np
 
 class CellCropper():
     
-    def __init__(self, img, csv, dirname='data', crop_size=64, batch_size=1_000):
-        self.img = img.astype(np.float32)
+    def __init__(self, img, csv, dirname='data', crop_size=32, batch_size=1_000, transform_fn=None):
+        self.img = img
+        self.n_channels = self.img.shape[0]
         self.csv = csv.reset_index(drop=True).astype(int)
-        self.csv.columns = ['x', 'y']
+        self.n_cells = self.csv.shape[0]
         self.dirname = dirname
         self.crop_size = crop_size
         self.offset = self.crop_size // 2
         self.batch_size = batch_size
+        self.transform_fn = transform_fn
 
     def crop(self):
         """Prepare dataset."""
         self.filter_cells()
         self.crop_cells()
+        if self.transform_fn:
+            self.thumbnails = self.transform_fn(self.thumbnails)
         os.makedirs(self.dirname, exist_ok=True)
         np.save(f'{self.dirname}/subset_idx.npy', self.csv.index.to_numpy())
         np.save(f'{self.dirname}/thumbnails.npy', self.thumbnails)
@@ -26,19 +30,19 @@ class CellCropper():
     def filter_cells(self):
         """Filter cells near image boundaries."""
         _, img_height, img_width = self.img.shape
+        x, y = self.csv.values.T
         self.csv = self.csv[
-            (self.csv.x > self.offset) & (self.csv.x < img_width - self.offset) &
-            (self.csv.y > self.offset) & (self.csv.y < img_height - self.offset)
+            (x > self.offset) & (x < img_width - self.offset) &
+            (y > self.offset) & (y < img_height - self.offset)
         ]
     
     def crop_cells(self):
         """Crop all cells."""
-        num_channels = self.img.shape[0]
-        self.thumbnails = np.zeros((len(self.csv), self.crop_size, self.crop_size, num_channels), dtype=np.uint16)
-        for i, (xcenter, ycenter) in enumerate(self.csv[['x', 'y']].values):
+        self.thumbnails = np.zeros((self.n_cells, self.n_channels, self.crop_size, self.crop_size), dtype=np.uint16)
+        for i, (xcenter, ycenter) in enumerate(self.csv.values):
             self.thumbnails[i] = self.crop_one(xcenter, ycenter)
             if i % 10_000 == 0:
-                logging.info(f'Cropping cell {i} of {len(self.csv)}.')
+                logging.info(f'Cropping cell {i} of {self.n_cells}.')
 
     def crop_one(self, xcenter, ycenter):
         """Crop one cell."""
--- cellvae/transform.py
+++ cellvae/transform.py
@@ -12,10 +12,13 @@ class Vignette:
     def __call__(self, x):
         return x * self.mask[None, :, :]
 
-def transform_channels(thumbnails, min_quant, max_quant):
-    """Transform IF data of shape N C X Y"""
-    thumbnails = np.log1p(thumbnails.astype(np.float32))
-    qmin, qmax = np.quantile(thumbnails, q=(min_quant, max_quant), axis=(0, 2, 3), keepdims=True)
-    thumbnails = (thumbnails - qmin) / (qmax - qmin)
-    return np.clip(thumbnails, 0, 1)
+class LogTransform:
 
+    def __init__(self, config):
+        self.qmin = config.preprocess.min_quant
+        self.qmax = config.preprocess.max_quant
+    
+    def __call__(self, x):
+        xmin, xmax = np.quantile(x, q=(self.qmin, self.qmax), axis=(0, 2, 3), keepdims=True)
+        x = (x - xmin) / (xmax - xmin)
+        return np.clip(x, 0, 1).astype(np.float32)
